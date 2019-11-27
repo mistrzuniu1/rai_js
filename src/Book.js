1dc56fab02574f1cd8fce4c2d7cbaee532a3989b
Bartlomiej Wroblewski <bwroblew@bwroblew-mac01.local>
2019-11-27 17:23:49 +0100

    Add book funcionality

--- src/Book.js
+++ src/Book.js
@@ -1,39 +1,57 @@
+var Borrowing = require('../src/Borrowing');
+
 module.exports = (class Book {
-    constructor(number, title, author, publishDate, price, borrowing){
+    constructor(number, title, author, publishDate, price){
         this.number = number;
         this.title = title;
         this.author = author;
-        this.publishDate = publishDate;
-        this.price = price;
-        this.borrowDate = borrowDate;
-        this.whoBorrowed = whoBorrowed;
+        this.borrowsList = [];
+        this.currentBorrowing = null;
     }
 
     returnBook(person) {
-        returnsList.push(person)
+        this.currentBorrowing = null;
     }
 
-    borrowBook(person){
-        borrowsList.push(person)
+    borrowBook(person, days){
+        var bookFrom = Date();
+        var bookTill = bookFrom;
+        bookTill.setDate(bookFrom + days);
+        this.currentBorrowing = new Borrowing(person, bookFrom, bookTill);
+        borrowsList.push(this.currentBorrowing);
     }
 
     prolong(duration){
-        
+        if (this.isAvailable())
+        {
+            return;
+        }
+        var currRetDate = this.currentBorrowing.toDate;
+        var newRetDate = this.currentBorrowing.toDate.setDate()
+        this.currentBorrowing.toDate
     }
 
     isAvailable(){
-
+        return (this.currentBorrowing == null);
     }
 
     isOutdated(){
-
+        return (!isAvailable() && this.currentBorrowing.toDate <= Date());
     }
 
     whenWillBeAvailable(){
-
+        if (this.isAvailable)
+        {
+            return Date();
+        }
+        return this.currentBorrowing.toDate;
     }
 
     getBorrower(){
-        
+        if (this.isAvailable())
+        {
+            return null;
+        }
+        return this.currentBorrowing.person;
     }
 });
