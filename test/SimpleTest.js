e868c3d6af09ee7e7ecc697ca69d434bb20d033f
Radek Slowinski <rslowinski97@gmail.com>
2019-11-27 17:58:49 +0100

    Test person

--- test/SimpleTest.js
+++ test/SimpleTest.js
@@ -1,16 +1,37 @@
 const expect = require('chai').expect;
+
 const Library = require('../src/Library');
 const Book = require('../src/Book');
 const Borrowing = require('../src/Borrowing');
-var Person = require('../src/Person');
+const Person = require('../src/Person');
 
-// import Person from "../src/Person"
+function createLibrary() {
+    const hamlet = new Book("#0", "Hamlet", "Sekspir");
+    const lalka = new Book("#1", "Lalka", "Prus");
+	return new Library([hamlet, lalka]);
+}
+function createPerson() {
+    return new Person("John", "Carmack");
+}
 
-		describe('canary-tests', function() 
-		{
-			it('should always pass the canary test', function() 
-			{
-				var person = new Person("John", "Obama")
-				expect(person.name).to.eql("John");
-			});
-		});
+describe('Person', function () {
+
+    it('should be able to borrow a book', function () {
+		const student = createPerson();
+		const lalka = createLibrary().getBook("Lalka");
+
+        expect(lalka.borrowBook(student, 4)).to.eq(true);
+
+        expect(lalka.currentBorrowing.person).to.eq(student)
+    });
+
+    it('should not be able to borrow a book that is already borrowed', function () {
+		const student = createPerson();
+		const student2 = createPerson();
+		const lalka = createLibrary().getBook("Lalka");
+
+		expect(lalka.borrowBook(student2, 3)).to.eq(false);
+		expect(lalka.currentBorrowing.person).to.eq(student);
+	});
+
+});
